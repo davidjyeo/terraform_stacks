38c8734aaf23328b024f59cd2f3ac73697010e22
David Yeo <david.j.yeo@outlook.com>
2024-10-31 09:07:39 +0000

    Initial Commit

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -12,5 +12,9 @@ terraform {
 provider "azuread" {}
 
 provider "azurerm" {
+  subscription_id = "efa8a235-ebc8-496b-8fb5-4ae931cc12bf"
+  # client_id       = "080c8627-412d-402e-80a8-3bed5c1f5de2"
+  # client_secret   = "0jI8Q~M~_79jd5RrUjm7mBPyfOX14cJtvCU0Xbgx"
+  # tenant_id       = "88ef261e-b19b-4d71-9afd-cdac31a6dcda"
   features {}
 }
