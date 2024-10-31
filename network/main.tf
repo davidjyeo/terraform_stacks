1e79bc5de7e7704f004eedab04193f4b0d383ea8
David Yeo <david.j.yeo@outlook.com>
2024-10-31 16:22:54 +0000

    azapi

--- network/main.tf
+++ network/main.tf
@@ -39,4 +39,4 @@ module "vnet" {
 #   resource_group_name  = azurerm_resource_group.main.name
 #   virtual_network_name = azurerm_virtual_network.main.name
 #   address_prefixes     = each.value
-}
+# }
