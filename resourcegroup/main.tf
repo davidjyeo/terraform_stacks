037dd3ac1f51d3e89fc1542d4e9fe022f685ee07
David Yeo <david.j.yeo@outlook.com>
2024-10-31 16:11:09 +0000

    azapi

--- resourcegroup/main.tf
+++ resourcegroup/main.tf
@@ -1,12 +1,15 @@
+data "azapi_client_config" "current" {}
+
 resource "azurerm_resource_group" "rg" {
   name     = "rg-tfc-stack"
   location = var.location
 }
 
 resource "azapi_resource" "rg" {
-  type     = "Microsoft.Resources/resourceGroups@2024-03-01"
-  name     = "rg-tfc-azapi" #"${module.naming.resource_group.name}-01"
-  location = var.location
+  type      = "Microsoft.Resources/resourceGroups@2024-03-01"
+  parent_id = data.azapi_client_config.current.subscription_id
+  name      = "rg-tfc-azapi" #"${module.naming.resource_group.name}-01"
+  location  = var.location
 }
 
 
